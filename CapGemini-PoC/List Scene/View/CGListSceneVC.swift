044d4594fc6447950bf8ac776c2a9b02d0e3edf0
Murugananda Mukesh <mmukesh.tecie@gmail.com>
2018-06-17 15:30:16 +0530

    List scene screen
    Custom cell class created
    Table view delegates and datasource added

--- CapGemini-PoC/List Scene/View/CGListSceneVC.swift
+++ CapGemini-PoC/List Scene/View/CGListSceneVC.swift
@@ -38,3 +38,22 @@ class CGListSceneVC: UIViewController{
     }
 }
 
+extension CGListSceneVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CGListSceneCell") as! CGListSceneCell
+                
+        return cell
+    }
+    
+    func  tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+       return 200
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+}
