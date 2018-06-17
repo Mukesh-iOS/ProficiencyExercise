58470e08cf0ce3e1aabd2409ac26e87d12443e63
Murugananda Mukesh <mmukesh.tecie@gmail.com>
2018-06-17 14:31:48 +0530

    Navigation controller added Welcome screen UI created App Transport security added

---
+++ CapGemini-PoC/Welcome Scene/CGWelcomeSceneVC.swift
@@ -0,0 +1,28 @@
+//
+//  CGWelcomeSceneVC.swift
+//  CapGemini-PoC
+//
+//  Created by murugananda on 17/06/18.
+//  Copyright © 2018 murugananda. All rights reserved.
+//
+
+import UIKit
+
+class CGWelcomeSceneVC: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
