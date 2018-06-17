ea4f526ff4af9be175ef39f3c2c489cf95143b26
Murugananda Mukesh <mmukesh.tecie@gmail.com>
2018-06-17 14:17:57 +0530

    Master commit
    Project created

---
+++ CapGemini-PoC/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CapGemini-PoC
+//
+//  Created by murugananda on 17/06/18.
+//  Copyright © 2018 murugananda. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
